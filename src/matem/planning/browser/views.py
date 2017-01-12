c272a0d40a0a2d3a4128cd063afe603102bdfadd
adriana-rv <adriana.rv@gmail.com>
2017-01-12 11:13:24 -0600

    Add condition display for file

--- src/matem/planning/browser/views.py
+++ src/matem/planning/browser/views.py
@@ -5,3 +5,8 @@ from plone.dexterity.browser.view import DefaultView
 
 class PlanView(DefaultView):
     """The default view for plans."""
+
+    def hasfile(self):
+        if self.context.file:
+            return True
+        return False
