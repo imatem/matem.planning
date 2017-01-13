81f4f1483d6b98b97ce006fbce939906748048a8
adriana-rv <adriana.rv@gmail.com>
2017-01-13 11:03:58 -0600

    Add masterselect plan type

--- src/matem/planning/content/plan.py
+++ src/matem/planning/content/plan.py
@@ -7,6 +7,24 @@ from plone.namedfile.field import NamedBlobFile
 from plone.supermodel import model
 from zope import schema
 
+from plone.formwidget.masterselect import MasterSelectField
+# from Products.Archetypes.utils import DisplayList
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.interface import directlyProvides
+
+
+def PlanTypesVocabulary(context):
+    items = [
+        (_(u'By Text'), 'plantext'),
+        (_(u'By File'), 'planfile'),
+    ]
+
+    items = [SimpleTerm(i[1], i[1], i[0]) for i in items]
+    return SimpleVocabulary(items)
+directlyProvides(PlanTypesVocabulary, IVocabularyFactory)
+
 
 class IPlan(model.Schema):
     """Dexterity Schema for Plans."""
@@ -15,6 +33,28 @@ class IPlan(model.Schema):
         title=_(u'Title'),
         required=True,
     )
+
+    plan_type = MasterSelectField(
+        title=_(u'label_plan_type', default=u'Plan type'),
+        vocabulary='matem.sis.PlanTypesVocabulary',
+        default='plantext',
+        required=True,
+        slave_fields=(
+            {
+                'name': 'text',
+                'action': 'hide',
+                'hide_values': ('planfile',),
+                'siblings': True,
+            },
+            {
+                'name': 'file',
+                'action': 'hide',
+                'hide_values': ('plantext',),
+                'siblings': True,
+            },
+        ),
+    )
+
 
     form.widget('text', cols=80, rows=20)
     text = schema.Text(
