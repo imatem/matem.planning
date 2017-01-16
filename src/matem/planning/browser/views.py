1332e2ec67908058c07c569655c8d04800b8f50c
adriana-rv <adriana.rv@gmail.com>
2017-01-16 14:57:54 -0600

    Add new file text to view

--- src/matem/planning/browser/views.py
+++ src/matem/planning/browser/views.py
@@ -7,6 +7,17 @@ class PlanView(DefaultView):
     """The default view for plans."""
 
     def hasfile(self):
-        if self.context.file:
-            return True
-        return False
+        if self.context.plan_type == 'plantext':
+            if self.context.textfile:
+                return True
+            return False
+        else:
+            if self.context.file:
+                return True
+            return False
+
+    def getFile(self):
+        if self.context.plan_type == 'plantext':
+            return self.context.textfile
+
+        return self.context.file
