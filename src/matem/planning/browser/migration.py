37c15ab0bfad20eb11c0343091aa848eef378a5b
Gilo <gil.cano@gmail.com>
2020-10-22 11:53:50 -0500

    planes 2021

--- src/matem/planning/browser/migration.py
+++ src/matem/planning/browser/migration.py
@@ -72,7 +72,7 @@ class MigrationForm(form.Form):
             review_state='active',
         )
         for brain in reserchers:
-            if brain.id != 'callejas':
+            if brain.id == 'callejas':
                 continue
             planfolder = self.getplanfolder(brain.id)
             if year in planfolder:
